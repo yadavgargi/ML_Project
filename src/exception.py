56abe9b30cd48def7d27ffe9965eb6b41eae38f5
yadavgargi <yadavgargi80@gmail.com>
2024-07-17 14:20:22 +0530

    logger and exceptions

--- src/exception.py
+++ src/exception.py
@@ -28,12 +28,7 @@ class CustomException(Exception):
    
 
 
-if __name__== "__main__":
-    try:
-      a=1/0
-    except Exception as e:
-       logging.info("divided by zero")
-       raise CustomException(e,sys)
+
 
 
    
